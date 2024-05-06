619d8682ace7830296887fca49b03744cc9105dd
itaboo <itaboo@itaboononotobukkukonpyuta.local>
2024-05-06 15:16:57 +0900

    commit.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,19 @@ import Header from '@/components/layouts/header/Header'
 import Footer from '@/components/layouts/footer/Footer'
 
 export default async function Home() {
- const data = await client.get({
+  const data = await client.get({
   endpoint: 'blogs',
- })
+  })
 
- return (
-  <>
-   <Header></Header>
-   <main>
-    {data.contents.map((content: { id: string }) => (
-     <div key={content.id}>{content.id}</div>
-    ))}
-   </main>
-   <Footer></Footer>
-  </>
- )
+  return (
+    <>
+    <Header></Header>
+    <main>
+      {data.contents.map((content: { id: string }) => (
+        <div key={content.id}>{content.id}</div>
+      ))}
+    </main>
+    <Footer></Footer>
+    </>
+  )
 }
